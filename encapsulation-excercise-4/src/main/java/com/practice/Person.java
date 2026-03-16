434e201fc98a331a3dbcd5c63ad39fab39e17efa
mphele <mphelemoswane3@gmail.com>
2026-03-16 13:50:58 +0200

    Excercise 4 added

---
+++ encapsulation-excercise-4/src/main/java/com/practice/Person.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+public class Person {
+
+    // TODO: private fields
+
+    // TODO: getters
+
+    // TODO: setters
+
+    // TODO: implement isAdult()
+
+    // TODO: implement calculateBMI()
+
+}
